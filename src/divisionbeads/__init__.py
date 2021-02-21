650e11ad58105260cdd3bd64975f677757cace04
Al Sweigart <asweigart@gmail.com>
2021-02-21 17:03:26 -0600

    Adding initial files.

---
+++ src/divisionbeads/__init__.py
@@ -0,0 +1,6 @@
+"""Division Beads
+By Al Sweigart al@inventwithpython.com
+
+A small Python CLI app of the "divison beads" math education tool."""
+
+__version__ = '0.1.0'
---
+++ src/divisionbeads/__main__.py
@@ -0,0 +1,4 @@
+import divisionbeads
+
+if __name__ == '__main__':
+    pass
